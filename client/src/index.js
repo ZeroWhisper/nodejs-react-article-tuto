a9b0c7484a1a40f026ad3ea03ee41b6c2e729915
Marcos <marcos.devel@gmail.com>
2019-05-03 09:43:06 -0300

    Atualizando o folder client com react, caminho absoluto dentro do projecto com webpack

--- client/src/index.js
+++ client/src/index.js
@@ -1,30 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Edit from "./components/Edit";
-import Create from "./components/Create";
-import Show from "./components/Show";
+import App from "App";
 
-import App from "./App";
+ReactDOM.render(<App />, document.getElementById("root"));
 
-const client = new ApolloClient();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
 
-ReactDOM.render(
-  <ApolloProvider client={client}>
-    {/* <App /> */}
-    <div>Teste</div>
-  </ApolloProvider>,
-  <Router>
-    <div>
-      <Route exact path="/" component={App} />
-      <Route path="/edit/:id" component={Edit} />
-      <Route path="/create" component={Create} />
-      <Route path="/show/:id" component={Show} />
-    </div>
-  </Router>,
-  document.getElementById("root")
-);
+// serviceWorker.unregister();
---
+++ client/src/pages/login/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import styled from "styled-components";
+
+export default () => {
+  return <div style={{ background: "#1A854D" }}>TESTE 222222</div>;
+};
